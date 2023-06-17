47868c2410b8b330dd03b201ba15965f75555201
wally-pacmero <74281685+wally-pacmero@users.noreply.github.com>
2023-06-16 19:39:29 -0500

    Update Main.php

--- src/DarkWav/SAC/Main.php
+++ src/DarkWav/SAC/Main.php
@@ -53,17 +53,17 @@ class Main extends PluginBase
     $this->Config = $this->getConfig();
     $this->advancedConfig = new Config($this->getDataFolder() . "advanced.yml", Config::YAML);
     $this->Colorized = $this->Config->get("Color"); #set color for output messages
-    $this->logger->info("[SAC] > ShadowAntiCheat enabled");
+    $this->getLogger()->info("[SAC] > ShadowAntiCheat enabled");
 
     #config integrity check
 
     if($this->Config->get("config_version") != $this->config_version || $this->advancedConfig->get("config_version") != $this->config_version) #check if the config is up to date.
     {
-      $this->logger->warning(TextFormat::YELLOW . "[SAC] > Your configuration is outdated, please update when possible"); #nofify user about outdated config
+      $this->getLogger()->warning(TextFormat::YELLOW . "[SAC] > Your configuration is outdated, please update when possible"); #nofify user about outdated config
     }
     if(!in_array($this->Config->get("plugin_version"), $this->supported_config_versions) || !in_array($this->advancedConfig->get("plugin_version"), $this->supported_config_versions)) #check if the config file is compatible with the current version of the plugin.
     {
-      $this->logger->error(TextFormat::RED . "[SAC] > Your configuration file is incompatible with this version of SAC, please delete ./plugin_data/ShadowAntiCheat/config.yml and ./plugin_data/ShadowAntiCheat/advanced.yml"); #throw error and nofify user about incompatible config
+      $this->getLogger()->error(TextFormat::RED . "[SAC] > Your configuration file is incompatible with this version of SAC, please delete ./plugin_data/ShadowAntiCheat/config.yml and ./plugin_data/ShadowAntiCheat/advanced.yml"); #throw error and nofify user about incompatible config
       $this->server->getPluginManager()->disablePlugin($this); #disable the plugin to prevent unpretendable errors
     }
     
